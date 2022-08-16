eff4f2af5ea3d3e46fb4da1867c16847a0f1ade2
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-08-16 20:44:47 +0300

    Добавьте файлы проекта.

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿--CREATE TABLE [dbo].[Themes] (
+--    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+--    [Name] NVARCHAR (200) NOT NULL,
+--    PRIMARY KEY CLUSTERED ([Id] ASC),
+--    CONSTRAINT [CK_Themes_Name] CHECK ([Name]<>'')
+--);
+
+--insert into [Themes] values ('T1'), ('T2')
+
+--select * from [Themes]
