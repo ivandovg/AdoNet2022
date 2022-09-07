2dcd27d4fd89ce4e366a78ebe0164ee6c9fd7d36
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-09-07 19:23:19 +0300

    Lesson 11-1

---
+++ AdoNet11_1/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNet11_1
+{
+    class Author
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Firstname} {Lastname} ({Country}), {Birthdate.ToShortDateString()}";
+        }
+    }
+}
---
+++ AdoNet11_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data;
+
+namespace AdoNet11_1
+{
+    class Program
+    {
+        private const string connectionStr = @"Data Source=DESKTOP-ALEU41I\SQLEXPRESS;Initial Catalog=BooksDb;Integrated Security=True";
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(connectionStr);
+            
+            //TestRead1(connection);
+            TestRead2(connection);
+
+            Console.WriteLine("\nPress any key...");
+            Console.ReadKey();
+        }
+
+        private static void TestRead1(IDbConnection connection)
+        {
+            using (connection)
+            {
+                connection.Open();
+                // run query
+                string sql = "select * from Authors";
+                var result = connection.Query(sql);
+                foreach (var a in result)
+                {
+                    Console.WriteLine($"{a.Firstname} {a.Lastname}, {a.Birthdate}");
+                }
+            }
+        }
+        private static void TestRead2(IDbConnection connection)
+        {
+            using (connection)
+            {
+                connection.Open();
+                // run query
+                string sql = "select * from Authors";
+                var result = connection.Query<Author>(sql);
+                foreach (var a in result)
+                {
+                    Console.WriteLine(a.ToString());
+                }
+            }
+        }
+    }
+}
